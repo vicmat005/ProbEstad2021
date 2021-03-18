3a9506a2e6fddb39a6a9fc00357790a495132829
Fernando Barrios <fbarrios@unam.mx>
2021-03-18 12:31:28 -0600

    Comment02

--- func/Ejemplo_norm.R
+++ func/Ejemplo_norm.R
@@ -5,8 +5,8 @@
 f_norm <- function(x){
   return((1/sqrt(2*pi)*exp((-x**2)/2)))
 }
-# Para gracicar con una línea
-curve(f_norm, # funcion Normal estándar que definí arriva
+# Para graficar con una línea
+curve(f_norm, # función Normal estándar que definí arriba
   from = -3.5, # Desde z = -3.5
   to = 3,  # Hasta z = 3.5
   ylab = "Probabilidad",
